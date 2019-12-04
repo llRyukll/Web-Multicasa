110f2eb99594787e8ef8cb69c014636cf1d32b40
llryukll <irvinggt07@gmail.com>
2019-12-04 16:59:02 -0600

    admin

---
+++ admin/inicio_privado.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio Publico</title>
+	<meta chars="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="../css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="css/estilosAdmin.css">
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap-grid.css"> 
+
+</head>
+<body>
+	<div class="contenedor">
+		<header class="header">
+			<div class="logo-empresa">
+				<img class="logotipo" src="../images/logo.png">
+				<img class="tel" src="../images/tel.png" >
+				<!--<h1>&nbsp;&nbsp;BIENES RAICES MULTICASA</h1>
+					<h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TU MEJOR OPCIÓN EN AGENCIA DE BIENES RAICES</h5>-->
+			</div>
+			<!--<div class="menuHorizontal">
+				<ul class="topnav">
+					<li class="derecha"><a href="#">Inicio</a></li>
+					<li class="derecha"><a href="#">La Compañia</a></li>
+					<li class="derecha"><a href="#">Acciones</a></li>
+					<li class="derecha"><a href="#">Reportes</a></li>
+					<li class="derecha"><a href="#">Admin</a></li>
+
+				</ul>
+			</div>-->
+			<!-------------------------- Menu desplegable  -------------------------------->
+			<br><br><br><br>
+			<div class="topnav" id="myTopnav">
+			  <a href="#">Inicio</a>
+			  <a href="#">La compañia</a>
+			  <a href="#">Contactos</a>
+			  <div class="dropdown">
+			    <button class="dropbtn">Acciones 
+			      <i class="fa fa-caret-down"></i>
+			    </button>
+			    <div class="dropdown-content">
+			      <a href="#">Insertar</a>
+			      <a href="#">Modificar</a>
+			      <a href="#">Borrar</a>
+			    </div>
+			  </div>
+			  <a href="#">Reportes</a>
+			  <a href="#" class="active">Admin</a>
+			  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="resposiveMenu()">&#9776;</a>
+			</div>
+
+
+
+			<!------------------------ Fin Menu desplegable  -------------------------------->
+
+		</header>
+
+		<main class="contenido">
+			<div class="slider">
+				<div class="w3-content w3-section" >
+					<img class="mySlides" src="../images/casa_1.png" style="width: 100%">
+					<img class="mySlides" src="../images/casa_2.jpg" style="width: 100%">
+					<img class="mySlides" src="../images/casa_3.jpg" style="width: 100%">
+				</div>
+			</div>
+			<img class="colash" src="../images/colash_min_casas.png">
+
+			<h1>BIENVENIDO</h1>
+			<h5>¡A nuestro sitio WEB MultiCasa!</h5>
+
+			<p>
+				Hello. In this tutorial i will show you how to create an elegant WordPress PSD theme. You can use this web template also for consulting agency templates, services websites, marketing website templates, business website templates, finance website templates. The design is very clean and can be modified very easy into a great template.
+
+
+				<br><br><br>
+
+				Hello. In this tutorial i will show you how to create an elegant WordPress PSD theme. You can use this web 
+				template also for consulting agency templates, services websites, marketing website templates, business 
+				website templates, finance website templates. The design is very clean and can be modified very easy into 
+				a great template..
+				<br><br>
+
+				Hello. In this tutorial i will show you how to create an elegant WordPress PSD theme. You can use this web 
+				template also for consulting agency templates, services websites, marketing website templates, business 
+				website templates, finance website templates. The design is very clean and can be modified very easy into 
+				a great template..
+
+				<br><br>
+				Hello. In this tutorial i will show you how to create an elegant WordPress PSD theme. You can use this web 
+				template also for consulting agency templates, services websites, marketing website templates, business 
+				website templates, finance website templates. The design is very clean and can be modified very easy into 
+				a great template..
+
+			</p>
+		</main>
+
+		<aside class="sidebar-1">
+			<div class="menuVertical">
+				<ul>
+					<li><a href="#">COMPRA</a></li>
+					<li><a href="#">CONSTRUIR</a></li>
+					<li><a href="#">VENTA</a></li>
+					<li><a href="#">MUDANZAS</a></li>
+					<li><a href="#">SEGUROS</a></li>
+					<li><a href="#">CONTACTOS</a></li>
+				</ul>
+			</div>
+		</aside>
+
+		<aside class="sidebar-2">
+			<div class="logo-lupa">
+				<img class="logo-lupa" src="../images/busca_casa.png">
+			</div>				
+		</aside>
+
+		<footer class="footer">
+			<div class="logo-empresa">
+				<img class="logo" src="../images/logotipo.png">
+			</div>
+			<div class="menuFooter">
+				<ul class="topnav1">
+					<li class="derecha"><a href="#">Inicio</a></li>
+					<h5 class="fotterh5">:</h5>
+					<li class="derecha"><a href="#"> Compra </a></li>
+					<h5 class="fotterh5">:</h5>
+					<li class="derecha"><a href="#"> onstruir </a></li>
+					<h5 class="fotterh5">:</h5>
+					<li class="derecha"><a href="#"> Venta </a></li>
+					<h5 class="fotterh5">:</h5>
+					<li class="derecha"><a href="#"> Mudanzas </a></li>
+					<h5 class="fotterh5">:</h5>
+					<li class="derecha"><a href="#"> Seguros </a></li>
+					<h5 class="fotterh5">:</h5>
+					<li class="derecha"><a href="#"> Contactos </a></li>
+					<div class="copyright">
+						<p>Bienes Raíces Multicasa © 2019</p>
+						<a href="">Politicas De Privacidad</a>				
+					</div>
+				</ul>
+			</div>
+		</footer>
+
+		</div>
+
+	</body>
+	</html>
+	
+<script src="../js/carrucel.js"></script>
+<script src="js/jsAdmin.js"></script>
+
