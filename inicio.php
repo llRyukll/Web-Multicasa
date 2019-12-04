7234b7938be2054bce0234a990be4a2947172926
llryukll <irvinggt07@gmail.com>
2019-12-04 11:58:54 -0600

    primer comit

---
+++ inicio.php
@@ -0,0 +1,160 @@
+	<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio Publico</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-grid.css">
+
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<div class="logo-empresa">
+				<img class="logotipo" src="images/logotipo.png">
+				<img class="tel" src="images/tel.png" >
+				<h1>&nbsp;&nbsp;BIENES RAICES MULTICASA</h1>
+				<h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TU MEJOR OPCIÓN EN AGENCIA DE BIENES RAICES</h5>
+			</div>
+			<div class="menuHorizontal">
+				<ul class="derecha">
+					<li><a href="#">Inicio</a></li>
+					<li><a href="#">La Compañia</a></li>
+					<li><a href="#">Servicios</a></li>
+					<li><a href="#">Requisitos</a></li>
+					<a href="#" class="btn btn-success btn-sm float-right">Inicio</a>
+					<a href="#" class="btn btn-success btn-sm float-right">Buscar</a>
+					<a href="#" class="btn btn-success btn-sm float-right">Admin</a>
+				</ul>
+			</div>
+		</div>
+
+		<div class="contenido">
+			<img class="casa" src="images/casa_inicio.png">
+			<img class="colash" src="images/colash_min_casas.png">
+
+			<h1>BIENVENIDO</h1>
+			<h5>¡A nuestro sitio WEB MultiCasa!</h5>
+
+			<p>
+				Hello. In this tutorial i will show you how to create an elegant WordPress PSD theme. You can use this web template also for consulting agency templates, services websites, marketing website templates, business website templates, finance website templates. The design is very clean and can be modified very easy into a great template.
+
+
+				<br><br><br>
+
+				Hello. In this tutorial i will show you how to create an elegant WordPress PSD theme. You can use this web 
+				template also for consulting agency templates, services websites, marketing website templates, business 
+				website templates, finance website templates. The design is very clean and can be modified very easy into 
+				a great template..
+				<br><br>
+
+				Hello. In this tutorial i will show you how to create an elegant WordPress PSD theme. You can use this web 
+				template also for consulting agency templates, services websites, marketing website templates, business 
+				website templates, finance website templates. The design is very clean and can be modified very easy into 
+				a great template..
+
+				<br><br>
+				Hello. In this tutorial i will show you how to create an elegant WordPress PSD theme. You can use this web 
+				template also for consulting agency templates, services websites, marketing website templates, business 
+				website templates, finance website templates. The design is very clean and can be modified very easy into 
+				a great template..
+
+			</p>
+		</div>
+
+		<div class="sidebar-1">
+			<div class="menuVertical">
+				<ul>
+					<li><a href="#">COMPRA</a></li>
+					<li><a href="#">CONSTRUIR</a></li>
+					<li><a href="#">VENTA</a></li>
+					<li><a href="#">MUDANZAS</a></li>
+					<li><a href="#">SEGUROS</a></li>
+					<li><a href="#">CONTACTOS</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="sidebar-2">
+			slidebar 2
+		</div>
+	</div>
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-9">
+  <div class="container-fluid text-center text-md-left">
+    <div class="row">
+      <div class="col-md-2 mt-md-0 mt-1">
+       	<img class="logotipo" src="images/logotipo.png">
+      </div>
+
+      <div class="col-md-1 mb-md-0 mb-1">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Inicio</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-1 mb-md-0 mb-1">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Comprar</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-1 mb-md-0 mb-1">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Construir</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-1 mb-md-0 mb-1">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Venta</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-1 mb-md-0 mb-1">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Mudanzas</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-1 mb-md-0 mb-1">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Seguros</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-1 mb-md-0 mb-1">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Contactos</a>
+          </li>
+        </ul>
+      </div>
+
+    </div><!-- Grid row -->
+  </div><!-- Footer Links -->
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href="https://github.com/RaizenBS7/">Max Garcia</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+
+
+</body>
+</html>
